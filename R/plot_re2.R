1b677cea341b87454abe05d64b43dd57d8212790
merrillrudd <merrillrudd@gmail.com>
2017-01-16 18:36:02 -0800

    alternate plot for relative errors

--- R/plot_re2.R
+++ R/plot_re2.R
@@ -1,4 +1,4 @@
-plot_re2 <- function(dirs, xlab, itervec, ...){
+plot_re2 <- function(dirs, itervec, ...){
 	esterr <- matrix(NA, nrow=length(itervec), ncol=length(dirs))
 	for(i in 1:length(itervec)){
 		rep1 <- readRDS(file.path(path1, itervec[i], "Report.rds"))
@@ -9,9 +9,8 @@ plot_re2 <- function(dirs, xlab, itervec, ...){
 		esterr[itervec[i],2] <- (log(rep2$SPR_t[length(rep2$SPR_t)])-log(true$SPR_t[length(true$SPR_t)]))
 		esterr[itervec[i],3] <- (log(rep3$SPR_t[length(rep3$SPR_t)])-log(true$SPR_t[length(true$SPR_t)]))
 	}
-	plot(x=1,y=1,type="n", xlim=c(0,length(dirs)+1), xaxs="i", yaxs="i", ...)
+	plot(x=1,y=1,type="n", xlim=c(0,length(dirs)+1), xaxs="i", yaxs="i", ..., cex.axis=2)
 	axis(2, las=2, cex.axis=2)
-	if(xaxt!="n") axis(1, at=1:length(dirs), labels=xlab, cex.axis=2)
 	abline(h=0, col=gray(0.4))
 	par(new=TRUE)
 	boxplot(esterr, xaxs="i", yaxs="i", xlim=c(0,length(dirs)+1), col="tomato", ...)
