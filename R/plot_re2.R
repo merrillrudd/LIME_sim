1d6958c78c1fc30739857df769874341ec657b52
merrillrudd <merrillrudd@gmail.com>
2017-01-16 18:31:50 -0800

    alternate plot for relative errors

--- R/plot_re2.R
+++ R/plot_re2.R
@@ -1,4 +1,4 @@
-plot_re2 <- function(dirs, xlab, ylim, itervec){
+plot_re2 <- function(dirs, xlab, ylim, itervec, yaxt){
 	esterr <- matrix(NA, nrow=length(itervec), ncol=length(dirs))
 	if(length(dirs)!=length(xlab)) stop("number of directories and xlabels must match")
 	for(i in 1:length(itervec)){
@@ -10,10 +10,10 @@ plot_re2 <- function(dirs, xlab, ylim, itervec){
 		esterr[itervec[i],2] <- (log(rep2$SPR_t[length(rep2$SPR_t)])-log(true$SPR_t[length(true$SPR_t)]))
 		esterr[itervec[i],3] <- (log(rep3$SPR_t[length(rep3$SPR_t)])-log(true$SPR_t[length(true$SPR_t)]))
 	}
-	plot(x=1,y=1,type="n", xlim=c(0,length(dirs)+1), ylim=ylim, xaxs="i", yaxs="i", xaxt="n", yaxt="n")
+	plot(x=1,y=1,type="n", xlim=c(0,length(dirs)+1), ylim=ylim, xaxs="i", yaxs="i", xaxt="n", yaxt=yaxt)
 	axis(2, las=2, cex.axis=2)
 	axis(1, at=1:length(dirs), labels=xlab, cex.axis=2)
 	abline(h=0, col=gray(0.4))
 	par(new=TRUE)
-	boxplot(esterr, xaxs="i", yaxs="i", xlim=c(0,length(dirs)+1), ylim=ylim, xaxt="n", yaxt="n", col="tomato")
+	boxplot(esterr, xaxs="i", yaxs="i", xlim=c(0,length(dirs)+1), ylim=ylim, xaxt="n", yaxt=yaxt, col="tomato")
 }
