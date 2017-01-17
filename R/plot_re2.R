700e141ba0a9bc3e4b14514992fec2096c51d6c3
merrillrudd <merrillrudd@gmail.com>
2017-01-16 18:34:19 -0800

    alternate plot for relative errors

--- R/plot_re2.R
+++ R/plot_re2.R
@@ -1,6 +1,5 @@
-plot_re2 <- function(dirs, xlab, ylim, itervec, yaxt){
+plot_re2 <- function(dirs, xlab, itervec, ...){
 	esterr <- matrix(NA, nrow=length(itervec), ncol=length(dirs))
-	if(length(dirs)!=length(xlab)) stop("number of directories and xlabels must match")
 	for(i in 1:length(itervec)){
 		rep1 <- readRDS(file.path(path1, itervec[i], "Report.rds"))
 		rep2 <- readRDS(file.path(path2, itervec[i], "Report.rds"))
@@ -10,10 +9,10 @@ plot_re2 <- function(dirs, xlab, ylim, itervec, yaxt){
 		esterr[itervec[i],2] <- (log(rep2$SPR_t[length(rep2$SPR_t)])-log(true$SPR_t[length(true$SPR_t)]))
 		esterr[itervec[i],3] <- (log(rep3$SPR_t[length(rep3$SPR_t)])-log(true$SPR_t[length(true$SPR_t)]))
 	}
-	plot(x=1,y=1,type="n", xlim=c(0,length(dirs)+1), ylim=ylim, xaxs="i", yaxs="i", xaxt="n", yaxt=yaxt)
+	plot(x=1,y=1,type="n", xlim=c(0,length(dirs)+1), xaxs="i", yaxs="i", ...)
 	axis(2, las=2, cex.axis=2)
-	axis(1, at=1:length(dirs), labels=xlab, cex.axis=2)
+	if(xaxt!="n") axis(1, at=1:length(dirs), labels=xlab, cex.axis=2)
 	abline(h=0, col=gray(0.4))
 	par(new=TRUE)
-	boxplot(esterr, xaxs="i", yaxs="i", xlim=c(0,length(dirs)+1), ylim=ylim, xaxt="n", yaxt=yaxt, col="tomato")
+	boxplot(esterr, xaxs="i", yaxs="i", xlim=c(0,length(dirs)+1), col="tomato", ...)
 }
