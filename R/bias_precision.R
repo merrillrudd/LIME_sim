702632032c1454efbc59901aac73e04dbdf5d0c8
merrillrudd <merrillrudd@gmail.com>
2017-02-10 09:15:13 -0800

    added functions for plotting est err

---
+++ R/bias_precision.R
@@ -0,0 +1,62 @@
+bias_precision <- function(dirs, itervec){
+
+	dev <- esterr <- matrix(NA, nrow=length(itervec), ncol=length(dirs))
+	converge <- matrix(1, nrow=length(itervec), ncol=length(dirs))
+	bound <- matrix(0, nrow=length(itervec), ncol=length(dirs))
+
+	for(m in 1:length(dirs)){
+		for(i in 1:length(itervec)){
+				if(file.exists(file.path(dirs[m], itervec[i], "high_final_gradient.txt"))){
+					converge[i,m] <- 0
+					next
+				}	
+
+			true <- readRDS(file.path(dirs[m], itervec[i], "True.rds"))
+
+			if(grepl("LBSPR", dirs[m])==FALSE){
+				rep <- readRDS(file.path(dirs[m], itervec[i], "Report.rds"))
+				sdrep <- readRDS(file.path(dirs[m], itervec[i], "Sdreport.rds"))
+
+				if(all(is.na(sdrep))){
+					converge[i,m] <- 0
+					next
+				}
+
+				if(rep$sigma_R==2){
+					bound[i,m] <- 1
+					next
+				}
+
+				if(rep$S50==log(0.001)){
+					bound[i,m] <- 1
+					next
+				}
+
+				dev[i,m] <- rep$SPR_t[length(rep$SPR_t)] - true$SPR_t[length(true$SPR_t)]	
+				esterr[i,m] <- log(rep$SPR_t[length(rep$SPR_t)]) - log(true$SPR_t[length(true$SPR_t)])
+			}
+			if(grepl("LBSPR", dirs[m])){
+				if(file.exists(file.path(dirs[m], itervec[i], "non_convergence.txt"))){
+					converge[i,m] <- 0
+					next
+				}
+				rep <- readRDS(file.path(dirs[m], itervec[i], "LBSPR_results.rds"))
+				dev[i,m] <- (rep$SPR[length(rep$SPR)] - true$SPR_t[length(true$SPR_t)])
+				esterr[i,m] <- log(rep$SPR[length(rep$SPR)]) - log(true$SPR_t[length(true$SPR_t)])
+			}	
+		}
+	}
+
+	bias <- sapply(1:length(dirs), function(x) median(abs(dev[,x]), na.rm=TRUE))
+	precision <- sapply(1:length(dirs), function(x) sqrt((1/length(which(is.na(dev[,x])==FALSE)))*sum(dev[,x]^2, na.rm=TRUE)))
+
+
+	Outs <- NULL
+	Outs$dev <- dev
+	Outs$esterr <- esterr 
+	Outs$bias <- bias
+	Outs$precision <- precision
+	Outs$bound <- bound
+	Outs$converge <- converge
+	return(Outs)
+}
--- R/plot_re.R
+++ R/plot_re.R
@@ -62,7 +62,7 @@ if(recalc==TRUE){
 
 nf <- length(compareToLH)
 nl <- length(lh_vec)
-# col_vec <- brewer.pal(3, "Set1")
+col_vec <- brewer.pal(3, "Set1")
 names <- gsub("_", "\n", modcombos[,"Data_avail"])
 bias <- sapply(1:length(dirs), function(x) median(abs(dev[,x]), na.rm=TRUE))
 precision <- sapply(1:length(dirs), function(x) sqrt((1/nrow(dev))*sum(dev[,x]^2, na.rm=TRUE)))
@@ -72,16 +72,16 @@ lime_dirs <- dirs[which(grepl("LBSPR",dirs)==FALSE)]
 par(mfrow=c(nf,nl), mar=c(0,0,0,0), omi=c(0.75,0.75,1,1))
 for(ff in 1:nf){
 	for(ll in 1:nl){
-		index <- which(grepl(compareToLH[ff],dirs) & grepl(lh_vec[ll],dirs))
+		index <- which(grepl(paste0(compareToLH[ff],"/"),dirs) & grepl(lh_vec[ll],dirs))
 		lime_index <- which(grepl(compareToLH[ff],lime_dirs) & grepl(lh_vec[ll],lime_dirs))
 		if(length(index)==1) xmax <- 1.5
 		if(length(index)>1) xmax <- ncol(esterr[,index]) + 0.5
 
-		# beanplot(as.data.frame(esterr[,index]), col=lapply(1:length(col_vec), function(x) c(col_vec[x],"black","black","black")), xaxt="n", yaxt="n", lwd=3, na.rm=TRUE, xaxs="i", yaxs="i", what=c(0,1,1,0), add=TRUE, beanlines="median", beanlinewd=3)
 		plot(x=1, y=1, type="n", xaxs="i", yaxs="i", xlab="", ylab="", xlim=c(0.5,xmax), ylim=ylim, xaxt="n", yaxt="n")
+		beanplot(as.data.frame(esterr[,index]), col=lapply(1:length(col_vec), function(x) c(col_vec[x],"black","black","black")), xaxt="n", yaxt="n", lwd=3, na.rm=TRUE, xaxs="i", yaxs="i", what=c(0,1,1,0), add=TRUE, beanlines="median", beanlinewd=3)
 		abline(h=0, lwd=5, col="red")
-		par(new=TRUE)
-		boxplot(esterr[,index], ylim=ylim, xaxt="n", yaxt="n", col="goldenrod", xlim=c(0.5, xmax), xaxs="i", yaxs="i")
+		# par(new=TRUE)
+		# boxplot(esterr[,index], ylim=ylim, xaxt="n", yaxt="n", col="goldenrod", xlim=c(0.5, xmax), xaxs="i", yaxs="i")
 		if(text==TRUE){
 			text(x=1:length(index), y=0.8*ylim[2], round(precision[index],3), cex=2, col="red")
 			text(x=1:length(index), y=0.7*ylim[2], round(bias[index],3), cex=2, col="blue")
