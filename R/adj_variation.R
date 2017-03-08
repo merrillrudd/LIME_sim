9605433cbe462f75ebd79868fc164c109b9c34cd
merrillrudd <merrillrudd@gmail.com>
2017-03-08 06:42:45 -0800

    adjust S95 for hake

--- R/adj_variation.R
+++ R/adj_variation.R
@@ -18,7 +18,7 @@ lh_med <- create_lh_list(vbk=0.21, linf=64.58, lwa=0.0245, lwb=2.790, S50=37.0, 
 
 ## F10.002, Fequil=0.025, Frate=0.15
 if(selex_param==1) S95 <- NULL
-if(selex_param==2) S95 <- 40
+if(selex_param==2) S95 <- 60
 if(mat_param==1) M95 <- NULL
 if(mat_param==2) M95 <- 60
 lh_long <- create_lh_list(vbk=0.14, linf=100, lwa=5e-6, lwb=3, S50=25.0, S95=S95, M50=47.4, M95=M95, M=0.15, selex_input="length", maturity_input="length", binwidth=1, t0=-0.01, CVlen=CVlen, SigmaC=SigmaC, SigmaI=SigmaI, SigmaR=SigmaR, SigmaF=SigmaF, R0=1,  h=1, qcoef=1e-5, F1=0.25, Fequil=0.25, Frate=0.15, Fmax=0.7, start_ages=0, rho=rho)
