4437c3410aa23d45900101091a97071770767a5e
merrillrudd <merrillrudd@gmail.com>
2017-04-27 16:21:54 -0700

    adjust max age

--- R/adj_variation.R
+++ R/adj_variation.R
@@ -10,21 +10,21 @@ if(mat_param==1) M95 <- NULL
 if(mat_param==2) M95 <- 25
 if(is.null(nbins)) bw <- binwidth
 if(is.null(nbins)==FALSE) bw <- floor(36.2/nbins)
-lh_short <- create_lh_list(vbk=0.87, linf=36.2, lwa=0.05970, lwb=2.754, S50=11.3, S95=11.3*Sdelta, M50=20.2, M95=M95, selex_input="length", maturity_input="length", M=1.49, binwidth=bw, t0=-0.01, CVlen=CVlen, SigmaC=SigmaC, SigmaI=SigmaI, SigmaR=SigmaR, SigmaF=SigmaF, R0=1,  h=1, qcoef=1e-5, F1=0.25, Fequil=0.25, Frate=0.2, Fmax=0.7, start_ages=0, rho=rho, nseasons=nseasons)
+lh_short <- create_lh_list(vbk=0.87, linf=36.2, lwa=0.05970, lwb=2.754, S50=11.3, S95=11.3*Sdelta, M50=20.2, M95=M95, selex_input="length", maturity_input="length", M=1.49, binwidth=bw, t0=-0.01, CVlen=CVlen, SigmaC=SigmaC, SigmaI=SigmaI, SigmaR=SigmaR, SigmaF=SigmaF, R0=1,  h=1, qcoef=1e-5, F1=0.25, Fequil=0.25, Frate=0.2, Fmax=0.7, start_ages=0, rho=rho, nseasons=nseasons, AgeMax=4)
 
 ## F1=0.009, Fequil=0.25, Frate=0.17
 if(mat_param==1) M95 <- NULL
 if(mat_param==2) M95 <- 35
 if(is.null(nbins)) bw <- binwidth
 if(is.null(nbins)==FALSE) bw <- floor(64.58/nbins)
-lh_med <- create_lh_list(vbk=0.21, linf=64.58, lwa=0.0245, lwb=2.790, S50=20, S95=20*Sdelta, M50=34, M95=M95, selex_input="length", maturity_input="length", M=0.43, binwidth=bw, t0=-0.01, CVlen=CVlen, SigmaC=SigmaC, SigmaI=SigmaI, SigmaR=SigmaR, SigmaF=SigmaF, R0=1,  h=1, qcoef=1e-5, F1=0.25, Fequil=0.25, Frate=0.17, Fmax=0.7, start_ages=0, rho=rho, nseasons=nseasons, AgeMax=15)
+lh_med <- create_lh_list(vbk=0.21, linf=64.58, lwa=0.0245, lwb=2.790, S50=20, S95=20*Sdelta, M50=34, M95=M95, selex_input="length", maturity_input="length", M=0.27, binwidth=bw, t0=-0.01, CVlen=CVlen, SigmaC=SigmaC, SigmaI=SigmaI, SigmaR=SigmaR, SigmaF=SigmaF, R0=1,  h=1, qcoef=1e-5, F1=0.25, Fequil=0.25, Frate=0.17, Fmax=0.7, start_ages=0, rho=rho, nseasons=nseasons, AgeMax=22)
 
 ## F10.002, Fequil=0.025, Frate=0.15
 if(mat_param==1) M95 <- NULL
 if(mat_param==2) M95 <- 60
 if(is.null(nbins)) bw <- binwidth
 if(is.null(nbins)==FALSE) bw <- floor(100/nbins)
-lh_long <- create_lh_list(vbk=0.13, linf=90, lwa=0.02635, lwb=2.9595, S50=25, S95=25*Sdelta, M50=50, M95=M95, M=0.18, selex_input="length", maturity_input="length", binwidth=bw, t0=-0.01, CVlen=CVlen, SigmaC=SigmaC, SigmaI=SigmaI, SigmaR=SigmaR, SigmaF=SigmaF, R0=1,  h=1, qcoef=1e-5, F1=0.25, Fequil=0.25, Frate=0.15, Fmax=0.7, start_ages=0, rho=rho, nseasons=nseasons)
+lh_long <- create_lh_list(vbk=0.13, linf=90, lwa=0.02635, lwb=2.9595, S50=25, S95=25*Sdelta, M50=50, M95=M95, M=0.18, selex_input="length", maturity_input="length", binwidth=bw, t0=-0.01, CVlen=CVlen, SigmaC=SigmaC, SigmaI=SigmaI, SigmaR=SigmaR, SigmaF=SigmaF, R0=1,  h=1, qcoef=1e-5, F1=0.25, Fequil=0.25, Frate=0.15, Fmax=0.7, start_ages=0, rho=rho, nseasons=nseasons, AgeMax=30)
 
 
 # par(mfrow=c(1,3))
